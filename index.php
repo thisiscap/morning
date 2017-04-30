da6c96e875e153276b8d6fc0ff63e7d4c66eef19
thisiscap <hello@christopherporter.co.uk>
2017-04-30 21:29:38 +0100

    Adding php to output current day of the week

--- index.php
+++ index.php
@@ -4,18 +4,22 @@
 // Define PHP variables here
   $pageTitle = "Today, a daily update";
   $pageDescription = "A simple webpage that updates me on somethings I like to know each day.";
+
+  // Use php to find the current date
+  $currentDate = cal_to_jd(CAL_GREGORIAN,date("m"),date("d"),date("Y"));
 ?>
 
 <html>
-  <!-- Pull in the head, info populated from the PHP variables above -->
+
   <head>
+    <!-- Pull in the head, info populated from the PHP variables above -->
     <?php include('_partials/head.php'); ?>
   </head>
 
   <body>
     <main>
 
-      Hey Chris, <span id="happySynonym"></span> Tuesday. It's 23 outside &amp; in an hour it will be raining. There is a strong north westerly wind with gusts of 14mph. A joke you say? What is red and smells like blue paint? Red paint.
+      Hey Chris, <span id="happySynonym"></span> <span><?php echo(jddayofweek($currentDate,1)); ?></span>. It's 23 outside &amp; in an hour it will be raining. There is a strong north westerly wind with gusts of 14mph. A joke you say? What is red and smells like blue paint? Red paint.
 
     </main>
   </body>
