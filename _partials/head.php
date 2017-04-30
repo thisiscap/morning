8f6cba9493c39da7a2b06785bf44a5c9677e0e9c
thisiscap <hello@christopherporter.co.uk>
2017-04-30 21:10:40 +0100

    Creating head partial

---
+++ _partials/head.php
@@ -0,0 +1,27 @@
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Defined in the PHP variables -->
+<title><?php echo $pageTitle ?></title>
+<meta name="description" content="<?php echo $pageDescription ?>">
+
+<!-- Opt out of Google indexing (no point, personal site) -->
+<meta name="robots" content="noindex">
+
+<!-- Link to CSS -->
+<link rel="stylesheet" href="/assets/morning-update.css">
+
+<!-- Favicons -->
+<!-- For IE 10 and below -->
+<!-- Place favicon.ico in the root directory - no tag necessary -->
+<!-- For IE 11, Chrome, Firefox, Safari, Opera -->
+<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/assets/favicon-96x96.png">
+
+<!-- Some Android stuff so I can save the site to my phone -->
+<meta name="theme-color" content="#change-me">
+<link rel="manifest" href="/assets/manifest.json">
+<meta name="mobile-web-app-capable" content="yes">
+<link rel="icon" sizes="192x192" href="/assets/highres-icon.png">
