a53175abcf88661036214ce6fc3062ea3b13f295
thisiscap <hello@christopherporter.co.uk>
2017-04-30 22:18:24 +0100

    Making more legible and adding Dark Sky credit

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 // Define PHP variables here
   $pageTitle = "Today, a daily update";
   $pageDescription = "A simple webpage that updates me on somethings I like to know each day.";
+  $userName = "Chris";
 
   // Use php to find the current date
   $currentDate = cal_to_jd(CAL_GREGORIAN,date("m"),date("d"),date("Y"));
@@ -18,10 +19,18 @@
 
   <body>
     <main>
-
-      Hey Chris, <span id="happySynonym"></span> <span><?php echo(jddayofweek($currentDate,1)); ?></span>. It's 23 outside &amp; in an hour it will be raining. There is a strong north westerly wind with gusts of 14mph. A joke you say? What is red and smells like blue paint? Red paint.
-
+      Hey
+      <span><?php echo $userName ?></span>,
+      <span id="happySynonym"><!-- Word output with JS --></span>
+      <span><?php echo(jddayofweek($currentDate,1)); ?></span>.
+      It's 23 outside &amp; in an hour it will be raining. There is a strong north westerly wind with gusts of 14mph. A joke you say? What is red and smells like blue paint? Red paint.
     </main>
+
+    <aside>
+      <!-- Add Dark Sky mandatory message -->
+      <a href="https://darksky.net/poweredby/">Powered by Dark Sky</a>
+    </aside>
+
   </body>
 
   <!-- Pull in scripts -->
