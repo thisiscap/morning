71acc3d4418e8a3c3070d0a52bcf6e14a722f795
thisiscap <hello@christopherporter.co.uk>
2017-04-30 21:10:50 +0100

    Creating scripts partial

---
+++ _partials/scripts.php
@@ -0,0 +1,15 @@
+
+<!-- Script to pull in a random word from the array and output to element-->
+<script>
+  var words = ["happy", "lucky", "joyful", "mellow", "elated", "pleasant", "cheerful", "rosy"];
+
+  var getRandomWord = function () {
+      return words[Math.floor(Math.random() * words.length)];
+  };
+
+  var word = getRandomWord();
+
+  console.log(word);
+
+  document.getElementById("happySynonym").textContent = word;
+</script>
