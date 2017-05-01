10b00496eed2eb7b011b2e0eb6c512c52916da0c
thisiscap <hello@christopherporter.co.uk>
2017-05-01 16:53:28 +0100

    Updating array of words

--- _partials/scripts.php
+++ _partials/scripts.php
@@ -1,7 +1,7 @@
 
 <!-- Script to pull in a random word from the array and output to element-->
 <script>
-  var words = ["happy", "lucky", "joyful", "mellow", "elated", "pleasant", "cheerful", "rosy"];
+  var words = ["happy", "joyful", "mellow", "pleasant", "cheerful", "rosy"];
 
   var getRandomWord = function () {
       return words[Math.floor(Math.random() * words.length)];
