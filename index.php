f2e2bbd902134875b8960fc8caa43daf9c2f5b9b
thisiscap <hello@christopherporter.co.uk>
2017-05-01 16:52:44 +0100

    Adding various outputs from Dark Sky API

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 
 <?php
-// Define PHP variables here
+  // Define PHP variables here
   $pageTitle = "Today, a daily update";
   $pageDescription = "A simple webpage that updates me on somethings I like to know each day.";
   $userName = "Chris";
@@ -18,17 +18,46 @@
   </head>
 
   <body>
+    <!-- Pull in Dark Sky API code for weather output -->
+    <?php include('_partials/dark-sky-api.php') ?>
+
+    <!-- Start the text block -->
     <main>
+
       Hey
       <span><?php echo $userName ?></span>,
       <span id="happySynonym"><!-- Word output with JS --></span>
       <span><?php echo(jddayofweek($currentDate,1)); ?></span>.
-      It's 23 outside &amp; in an hour it will be raining. There is a strong north westerly wind with gusts of 14mph. A joke you say? What is red and smells like blue paint? Red paint.
+
+      It's <span><?php echo round($weather['currently']['temperature']) ?>&deg;</span> outside &amp; in an hour it will be raining.
+
+      There is a <span><?php echo round($weather['currently']['windSpeed']) ?>mph</span> wind coming from the
+
+      <span>
+      <?php
+        // Found this to echo into a variable (echo value is nummeral degree, i.e 123)
+        ob_start();
+        echo $weather['currently']['windBearing'];
+        $windBearing = ob_get_contents();
+        ob_end_clean();
+
+        // Put it into something more understandable
+        if (($windBearing >= "0") && ($windBearing <= "45")) { echo "North."; }
+        elseif (($windBearing >= "45") && ($windBearing <= "90")) { echo "North East."; }
+        elseif (($windBearing >= "90") && ($windBearing <= "135")) { echo "East."; }
+        elseif (($windBearing >= "135") && ($windBearing <= "180")) { echo "South East."; }
+        elseif (($windBearing >= "180") && ($windBearing <= "225")) { echo "South."; }
+        elseif (($windBearing >= "225") && ($windBearing <= "270")) { echo "South West."; }
+        elseif (($windBearing >= "270") && ($windBearing <= "315")) { echo "West."; }
+        else { echo "North West."; }
+      ?>
+      </span>
+
     </main>
 
+    <!-- Add Dark Sky mandatory message -->
     <aside>
-      <!-- Add Dark Sky mandatory message -->
-      <a href="https://darksky.net/poweredby/">Powered by Dark Sky</a>
+      <a href="https://darksky.net/poweredby/" title="Check out the Dark Sky website.">Powered by Dark Sky</a>
     </aside>
 
   </body>
